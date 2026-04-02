a6a5e9d8984c089fbf15374ee046290044938747
deepthithiru <deepthideepthi1452@gmail.com>
2026-04-02 21:30:47 +0530

    Update Charts.jsx

--- Employee tracker/components/Charts.jsx
+++ Employee tracker/components/Charts.jsx
@@ -1,1 +1,18 @@
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
+function Charts({ data }) {
+  return (
+    <div style={{ width: '100%', height: 300 }}>
+      <ResponsiveContainer>
+        <BarChart data={data}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="score" fill="#3b82f6" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default Charts; 
